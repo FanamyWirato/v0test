6c6eb1ed8c95d9dc8dcb5bcb5ba395c4025e1ee9
Fanamy Wirato <p.berger+github@posteo.de>
2021-03-20 14:35:11 +0100

    feat(vue) changed from dist to public output

--- src/Server/Network/Webserver.mjs
+++ src/Server/Network/Webserver.mjs
@@ -55,7 +55,7 @@ export default class Webserver {
             console.log(req.session); //if undefined, no valid session found!
 
             // if no valid session is found or no valid authentication happened, deliver login
-            res.sendFile(path.resolve(process.env.NODE_PATH + '/public/login.html'));
+            res.sendFile(path.resolve(process.env.NODE_PATH + '/src/WebApp/dist/index.html'));
         });
 
         this.http.listen(process.env.WEBSERVER_PORT);
